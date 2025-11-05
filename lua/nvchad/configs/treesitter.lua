edf8e6fa5aa29fdbff7088e97621fc613807828e
MohamoudAli12 <63305673+MohamoudAli12@users.noreply.github.com>
2025-11-05 16:33:16 +1100

    rust tools added

--- lua/nvchad/configs/treesitter.lua
+++ lua/nvchad/configs/treesitter.lua
@@ -4,7 +4,7 @@ pcall(function()
 end)
 
 return {
-  ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" , "python", "c" },
+  ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc", "python", "c", "rust", "zig" },
 
   highlight = {
     enable = true,
